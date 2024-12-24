ec4b26af569b17788725cba09bd5de215a8908c0
cite21094 <cite21094@g.nihon-u.ac.jp>
2024-12-24 12:11:14 +0900

    add

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -6,11 +6,13 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-//import java.util.Scanner;
+import java.util.Scanner;
 import java.util.List;
 
 public class Lox {
-    static boolean hadError = false;                  
+  private static final Interpreter interpreter = new Interpreter();
+    static boolean hadError = false;
+    static boolean hadRuntimeError = false;               
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -27,6 +29,7 @@ public class Lox {
     run(new String(bytes, Charset.defaultCharset()));
 
     if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
   }
 
   private static void runPrompt() throws IOException {
@@ -47,12 +50,17 @@ public class Lox {
     List<Token> tokens = scanner.scanTokens();
 
     Parser parser = new Parser(tokens);
-    Expr expression = parser.parse();
+    //Expr expression = parser.parse();
+    List<Stmt> statements = parser.parse();
 
     // Stop if there was a syntax error.
     if (hadError) return;
 
-    System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(statements);
+
+    //interpreter.interpret(expression);
+
+    //System.out.println(new AstPrinter().print(expression));
     
     // For now, just print the tokens.
     for (Token token : tokens) {
@@ -63,6 +71,12 @@ public class Lox {
 
   static void error(int line, String message) {
     report(line, "", message);
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 
   private static void report(int line, String where,String message) {
